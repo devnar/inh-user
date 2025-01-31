d958ca8055802ffd1b4a75f4904e157b70481a6c
NAR <88266823+devnar@users.noreply.github.com>
2025-02-01 00:29:07 +0300

    Update 157773871.js

--- 157773871.js
+++ 157773871.js
@@ -2,5 +2,6 @@ const dmUser = ["devnar","anonimgunluk"];
 inhget("https://raw.githubusercontent.com/devnar/inh/refs/heads/main/assets/scripts/math.js");
 inhget("https://raw.githubusercontent.com/devnar/inh/refs/heads/main/assets/scripts/dm.js");
 inhget("https://raw.githubusercontent.com/devnar/inh/refs/heads/main/assets/scripts/search.js");
+inhget("https://raw.githubusercontent.com/devnar/inh/refs/heads/main/assets/scripts/neofetch.js");
 color("#23b5b5");
-echo("<pre> IIIII  NN    NN HH   HH <br>  III   NNN   NN HH   HH <br>  III   NN N  NN HHHHHHH <br>  III   NN  N NN HH   HH <br> IIIII  NN    NN HH   HH </pre>",true)
+neofetch();
